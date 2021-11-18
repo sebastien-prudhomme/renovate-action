54c10a43daa416b27d9cf9e286a94a30870947b1
Sébastien Prud'homme <sebastien.prudhomme@gmail.com>
2021-11-18 22:33:49 +0100

    WIP

--- tests/test_all.py
+++ tests/test_all.py
@@ -2,6 +2,7 @@ import pytest
 import subprocess
 import testinfra
 
+
 @pytest.fixture(scope='session')
 def host(request):
     docker_id = subprocess.check_output(['docker', 'run', '-d', 'ghcr.io/sebastien-prudhomme/chart-tests:test1']).decode().strip()
@@ -9,6 +10,7 @@ def host(request):
     yield testinfra.get_host("docker://" + docker_id)
 
     subprocess.check_call(['docker', 'rm', '-f', docker_id])
+
 
 def test_pytest(host):
     assert host.check_output(['pytest', '-V']) == 'pytest 6.2.5'
