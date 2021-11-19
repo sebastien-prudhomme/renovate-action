616e2bc3c3d8e0c32a3ea3a75d7404694927f9c5
Sébastien Prud'homme <sebastien.prudhomme@gmail.com>
2021-11-19 22:53:19 +0100

    WIP

--- tests/test_all.py
+++ tests/test_all.py
@@ -1,3 +1,4 @@
+import re
 import subprocess
 
 import pytest
@@ -5,7 +6,7 @@ import testinfra
 
 
 @pytest.fixture(scope="session")
-def host(request):
+def host():
     image = "ghcr.io/sebastien-prudhomme/chart-tests:test1"
 
     docker_id = subprocess.check_output(["docker", "run", "-d", image, "sleep", "infinity"]).decode().strip()
@@ -14,7 +15,26 @@ def host(request):
 
     subprocess.check_call(["docker", "rm", "-f", docker_id])
 
+@pytest.fixture(scope="session")
+def packages(host):
+    return host.pip.get_packages()
 
-def test_pytest(host):
-    assert host.check_output('pytest -V') == 'pytest 1.0.0'
-    assert "pytest" in host.pip.get_packages()
+def test_grpcio_package(packages):
+    assert "grpcio" in packages
+
+def test_grpcio_health_checking_package(packages):
+    assert "grpcio-health-checking" in packages
+
+def test_mariadb_package(packages):
+    assert "mariadb" in packages
+
+def test_pytest_package(packages):
+    assert "pytest" in packages
+
+def test_pytest_version(host):
+    output = host.check_output("pytest -V 2>&1")
+
+    assert re.match(r"^pytest \d+\.\d+\.\d+$",  output)
+
+def test_requests_package(packages):
+    assert "requests" in packages
